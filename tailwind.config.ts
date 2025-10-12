974e1d80e9be59b3dfa2ce75b0079d07211b06c6
pansuntours-dotcom <pansuntours@gmail.com>
2025-10-11 20:16:31 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -42,7 +42,7 @@ const config: Config = {
           '100%': { backgroundPosition: '0% 50%' },
         },
         'pulse-glow': {
-          '0%, 100%': { opacity: 1, transform: 'scale(1)' },
+          '0%, 100%': { opacity: '1', transform: 'scale(1)' },
           '50%': { opacity: 0.8, transform: 'scale(1.05)' },
         },
       },
