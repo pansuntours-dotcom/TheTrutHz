cca49ac176fe6a426087577294b7a395f306bfa5
pansuntours-dotcom <pansuntours@gmail.com>
2025-10-20 16:17:36 -0400

    Update MotionDiv.tsx

--- src/components/MotionDiv.tsx
+++ src/components/MotionDiv.tsx
@@ -3,7 +3,6 @@
 import { motion, HTMLMotionProps } from "framer-motion";
 import React from "react";
 
-// Fully typed motion div wrapper
 const MotionDiv: React.FC<HTMLMotionProps<"div">> = (props) => {
   return <motion.div {...props} />;
 };
