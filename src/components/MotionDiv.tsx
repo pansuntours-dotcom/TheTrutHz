5531538f4a9d8039ac43c15d795166a4f726e1d8
pansuntours-dotcom <pansuntours@gmail.com>
2025-10-20 17:21:26 -0400

    Update MotionDiv.tsx

--- src/components/MotionDiv.tsx
+++ src/components/MotionDiv.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { motion, HTMLMotionProps } from "framer-motion";
-import React from "react";
+import React, { PropsWithChildren } from "react";
 
-const MotionDiv: React.FC<HTMLMotionProps<"div">> = (props) => {
-  return <motion.div {...props} />;
+// Correctly typed MotionDiv that allows all div props including className
+type MotionDivProps = PropsWithChildren<HTMLMotionProps<"div">>;
+
+const MotionDiv: React.FC<MotionDivProps> = ({ children, ...props }) => {
+  return <motion.div {...props}>{children}</motion.div>;
 };
 
 export default MotionDiv;
