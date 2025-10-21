69d24760ca4059c81bc063f62cd9aeb2330794ff
pansuntours-dotcom <pansuntours@gmail.com>
2025-10-21 07:08:48 -0400

    Update page.tsx

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { MotionSection } from "@/components/MotionDiv";
 
 export default function ContactPage() {
   return (
-    <motion.section
+    <MotionSection
       initial={{ opacity: 0, y: 30 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.6 }}
@@ -62,6 +62,6 @@ export default function ContactPage() {
           Send Message
         </button>
       </form>
-    </motion.section>
+    </MotionSection>
   );
 }
