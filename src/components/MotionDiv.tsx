3dbe606ebb09f9144c3d546f4f39a29244dd97a4
pansuntours-dotcom <pansuntours@gmail.com>
2025-10-20 20:17:21 -0400

    Update MotionDiv.tsx

--- src/components/MotionDiv.tsx
+++ src/components/MotionDiv.tsx
@@ -1,17 +1,10 @@
 "use client";
 
-import { motion } from "framer-motion";
-import React from "react";
+import { motion, MotionProps } from "framer-motion";
+import { HTMLAttributes } from "react";
 
-type MotionDivProps = React.HTMLAttributes<HTMLDivElement> & {
-  initial?: Record<string, any>;
-  animate?: Record<string, any>;
-  transition?: Record<string, any>;
-};
+type MotionDivProps = HTMLAttributes<HTMLDivElement> & MotionProps;
 
-export default function MotionDiv({
-  children,
-  ...rest
-}: MotionDivProps) {
+export default function MotionDiv({ children, ...rest }: MotionDivProps) {
   return <motion.div {...rest}>{children}</motion.div>;
 }
