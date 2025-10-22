868fe1e8d721444fce02d85f7b8869f9c2cca028
pansuntours-dotcom <pansuntours@gmail.com>
2025-10-21 22:40:06 -0400

    Update Motion.tsx

--- src/components/Motion.tsx
+++ src/components/Motion.tsx
@@ -1,10 +1,3 @@
 "use client";
-
-import { motion, MotionProps } from "framer-motion";
-import { HTMLAttributes } from "react";
-
-type MotionDivProps = MotionProps & HTMLAttributes<HTMLDivElement>;
-type MotionSectionProps = MotionProps & HTMLAttributes<HTMLElement>;
-
-export const MotionDiv = motion.div as React.FC<MotionDivProps>;
-export const MotionSection = motion.section as React.FC<MotionSectionProps>;
+import { motion } from "framer-motion";
+export default motion;
