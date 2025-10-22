7f5a4054d89367e48760a3f55d82fd705b58fdb4
pansuntours-dotcom <pansuntours@gmail.com>
2025-10-22 00:33:38 -0400

    Update page.tsx

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import MotionDiv from "@/components/MotionDiv";
+import MotionDiv from "@/components/Motion";
 
 export default function JoinPage() {
   return (
