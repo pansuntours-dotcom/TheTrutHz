636864d4c1ec3aa49e3bc9e7dae16ecc2cdea8f2
pansuntours-dotcom <pansuntours@gmail.com>
2025-10-23 15:46:43 -0400

    Update SupabaseClient.ts

--- lib/SupabaseClient.ts
+++ lib/SupabaseClient.ts
@@ -1,11 +1,13 @@
 // lib/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 if (!url || !anon) {
-  throw new Error('Missing Supabase env vars NEXT_PUBLIC_SUPABASE_URL or NEXT_PUBLIC_SUPABASE_ANON_KEY');
+  throw new Error(
+    'Missing Supabase env vars: NEXT_PUBLIC_SUPABASE_URL or NEXT_PUBLIC_SUPABASE_ANON_KEY'
+  )
 }
 
-export const supabase = createClient(url, anon);
+export const supabase = createClient(url, anon)
