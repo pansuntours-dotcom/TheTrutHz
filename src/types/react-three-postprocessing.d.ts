bf5281c1648a66566c2cba5b0ff73a396c9592e9
pansuntours-dotcom <pansuntours@gmail.com>
2025-10-24 11:19:20 -0400

    Create react-three-postprocessing.d.ts

---
+++ src/types/react-three-postprocessing.d.ts
@@ -0,0 +1,9 @@
+declare module '@react-three/postprocessing' {
+  const content: any;
+  export const EffectComposer: any;
+  export const Bloom: any;
+  export const DepthOfField: any;
+  export const Noise: any;
+  export const Vignette: any;
+  export default content;
+}
