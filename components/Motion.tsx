9630b4d327f09fe5411533b7deca3fc6e9f45df4
pansuntours-dotcom <pansuntours@gmail.com>
2025-10-24 07:20:33 -0400

    Create Motion.tsx

---
+++ components/Motion.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export const MotionDiv = motion.div;
+export const MotionSpan = motion.span;
+export const MotionSection = motion.section;
+
+export default motion;
