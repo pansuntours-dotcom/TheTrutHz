672b86e42076b463102fdef83225f1917c282df8
pansuntours-dotcom <pansuntours@gmail.com>
2025-10-25 17:26:25 -0400

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,9 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Combine class names with Tailwind Merge + clsx
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
