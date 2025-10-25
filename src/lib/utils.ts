b59d90aad4ac190d184a0daba618b8068d4024e0
pansuntours-dotcom <pansuntours@gmail.com>
2025-10-25 10:02:39 -0400

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
+// src/lib/utils.ts
+
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Utility function to merge Tailwind CSS classes intelligently.
+ * Combines clsx's conditional class merging with twMerge's deduplication.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
