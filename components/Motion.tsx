7b854cbb8051ac0f50dd18925de78261a854884e
pansuntours-dotcom <pansuntours@gmail.com>
2025-10-26 01:22:24 -0400

    Update Motion.tsx

--- components/Motion.tsx
+++ components/Motion.tsx
@@ -4,6 +4,3 @@ import { motion } from "framer-motion";
 
 export const MotionDiv = motion.div;
 export const MotionSpan = motion.span;
-export const MotionSection = motion.section;
-
-export default motion;
