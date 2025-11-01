0f23ececbe26d9e147ab8ddc4b121656ae34221e
pansuntours-dotcom <pansuntours@gmail.com>
2025-11-01 18:04:57 -0400

    Update LivePlayer.tsx

--- components/LivePlayer.tsx
+++ components/LivePlayer.tsx
@@ -1,13 +1,16 @@
+// components/LivePlayer.tsx
 'use client';
 
 import React, { useEffect, useRef } from 'react';
 import Hls from 'hls.js';
 
 interface LivePlayerProps {
-  url: string;
+  src: string; // HLS stream URL
+  width?: number;
+  height?: number;
 }
 
-const LivePlayer: React.FC<LivePlayerProps> = ({ url }) => {
+const LivePlayer: React.FC<LivePlayerProps> = ({ src, width = 640, height = 360 }) => {
   const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
@@ -16,32 +19,24 @@ const LivePlayer: React.FC<LivePlayerProps> = ({ url }) => {
 
     if (Hls.isSupported()) {
       const hls = new Hls();
-      hls.loadSource(url);
+      hls.loadSource(src);
       hls.attachMedia(video);
       hls.on(Hls.Events.MANIFEST_PARSED, () => {
-        video.play().catch(() => {});
+        video.play().catch(err => console.error('Video play error:', err));
       });
 
       return () => {
         hls.destroy();
       };
     } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-      video.src = url;
-      video.addEventListener('loadedmetadata', () => {
-        video.play().catch(() => {});
-      });
+      video.src = src;
+      video.play().catch(err => console.error('Video play error:', err));
+    } else {
+      console.error('HLS not supported in this browser');
     }
-  }, [url]);
+  }, [src]);
 
-  return (
-    <video
-      ref={videoRef}
-      controls
-      className="w-full mt-2 rounded-lg"
-      playsInline
-      preload="metadata"
-    />
-  );
+  return <video ref={videoRef} width={width} height={height} controls />;
 };
 
 export default LivePlayer;
