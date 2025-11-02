a8aefaf8db622f412e33d2bde16fad35d41a396c
pansuntours-dotcom <pansuntours@gmail.com>
2025-11-02 09:41:43 -0500

    Update and rename lib/supabaseClient.ts to lib/supabase/client.ts

--- lib/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-// lib/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js';
-
-// Ensure these environment variables are set in Netlify
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
