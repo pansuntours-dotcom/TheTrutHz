de290d1e354a322b99e4a13ce26f44daeb86abfb
pansuntours-dotcom <pansuntours@gmail.com>
2025-11-02 23:52:24 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,9 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
-};
+  plugins: [],
+}
