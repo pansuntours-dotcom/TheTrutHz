155ee82182afbb8777024481de3b60feff1d9793
pansuntours-dotcom <pansuntours@gmail.com>
2025-11-02 23:07:23 -0500

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,7 @@
 // lib/db.ts
 import { createClient } from '@supabase/supabase-js';
-import type { Database } from '../types/supabase';
 
-// Use your actual Supabase credentials
-const supabaseUrl = 'https://xiojphoyqjvcmsocgozu.supabase.co';
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhpb2pwaG95cWp2Y21zb2Nnb3p1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5NDc0NjgsImV4cCI6MjA3NzUyMzQ2OH0.6nNuLUqa9EBcVS1EeD-glISFhFqBEHTS3gxcrLuQUkc';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-// Create a typed Supabase client using your Database schema
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
